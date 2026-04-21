8fed80dc5dfea2296ef057ddf8fc537b9d434129
777Makss777 <777docmax777@gmail.com>
2026-04-21 14:47:56 +0200

    ...

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,56 +1,18 @@
 import { NextResponse } from 'next/server';
 import { api } from '../../api';
 import { cookies } from 'next/headers';
-import { parse } from 'cookie';
 import { isAxiosError } from 'axios';
 import { logErrorResponse } from '../../_utils/utils';
 
 export async function POST() {
   const cookieStore = await cookies();
-  const accessToken = cookieStore.get('accessToken')?.value;
-  const refreshToken = cookieStore.get('refreshToken')?.value;
 
   try {
-    if (accessToken || refreshToken) {
-      const cookieParts = [];
-
-      if (accessToken) {
-        cookieParts.push(`accessToken=${accessToken}`);
-      }
-
-      if (refreshToken) {
-        cookieParts.push(`refreshToken=${refreshToken}`);
-      }
-
-      const apiRes = await api.post(
-        '/auth/logout',
-        null,
-        {
-          headers: {
-            Cookie: cookieParts.join('; '),
-          },
-        },
-      );
-
-      const setCookie = apiRes.headers['set-cookie'];
-
-      if (setCookie) {
-        const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-        for (const cookieStr of cookieArray) {
-          const parsed = parse(cookieStr);
-          const options = {
-            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-            path: parsed.Path,
-            maxAge: Number(parsed['Max-Age']),
-          };
-          if (parsed.accessToken)
-            cookieStore.set('accessToken', parsed.accessToken, options);
-          if (parsed.refreshToken)
-            cookieStore.set('refreshToken', parsed.refreshToken, options);
-        }
-      }
-    }
-
+    await api.post('/auth/logout', null, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
     cookieStore.delete('accessToken');
     cookieStore.delete('refreshToken');
 
