6ff284af66f9634a85dfe21191a04de7d2c435df
777Makss777 <777docmax777@gmail.com>
2026-04-21 14:52:50 +0200

    ...

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -6,11 +6,13 @@ import { logErrorResponse } from '../../_utils/utils';
 
 export async function POST() {
   const cookieStore = await cookies();
+  const accessToken = cookieStore.get('accessToken')?.value;
+  const refreshToken = cookieStore.get('refreshToken')?.value;
 
   try {
-    await api.post('/auth/logout', null, {
+    await api.post('auth/logout', null, {
       headers: {
-        Cookie: cookieStore.toString(),
+        Cookie: `accessToken=${accessToken}; refreshToken=${refreshToken}`,
       },
     });
     cookieStore.delete('accessToken');
